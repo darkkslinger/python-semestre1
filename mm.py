cf1566499bbd740798e176293eaa7e2907dfd3fe
Daniel Perez <darkkslinger@users.noreply.github.com>
2023-04-06 14:18:30 -0400

    clase 5 abril

--- mm.py
+++ mm.py
@@ -1,3 +1,23 @@
-a
-a
-a
+print("hola soy Daniel")
+
+nombre = "Daniel"
+name = "Santiago"
+
+
+print(nombre)
+print("hola soy",nombre)
+
+
+edad = 17
+
+print("mi edad es de", edad)
+
+print("hola mi nombre es" " " + nombre + " "  "y tengo" + str(edad) + "años de edad")
+
+
+nombre = "Valentin"
+print("Hola mi nuevo nombre es", nombre)
+nombre = input("¿Cual es tu nombre?\n")
+print("tu nombre es:",nombre)
+
+print ("hola mi nombre nuevo es",nombre)
