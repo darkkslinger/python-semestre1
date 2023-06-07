877be843c9ec7436e83375b6cc8607615bb68f9a
Daniel Aliquintui <darkkslinger@users.noreply.github.com>
2023-06-07 00:37:37 -0400

    .

---
+++ guías 2/ejercicio2.py
@@ -0,0 +1,33 @@
+def obtener_numeros(cantidad):
+    numeros = []
+    for i in range (cantidad):
+        numero=float(input(f"ingrese el numero {i+1}: "))
+        numeros.append(numero)
+    return numeros
+
+def sumanumeros(n):
+    suma= sum(n)
+    return suma
+
+def sumapar(n):
+    suma = 0
+    for num in n:
+        if num%2==0:
+            suma+=num
+    return suma
+    
+def sumainpar(n):
+    suma = 0
+    for num in n:
+        if num%2!=0:
+            suma+=num
+    return suma
+
+c_n = int(input("¿Cuántos números desea operar?: "))
+nums = obtener_numeros(c_n) 
+sumanum=sumanumeros(nums)
+print(f"La suma de todos los números es: {sumanum}")
+sumapares=sumapar(nums)
+print(f"La suma de todos los números pares es: {sumapares}")
+sumaimpares=sumainpar(nums)
+print(f"La suma de todos los números impares es: {sumaimpares}")
